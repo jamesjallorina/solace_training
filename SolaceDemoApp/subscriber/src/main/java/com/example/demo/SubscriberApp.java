cc90cb701e91a6652a130af2da5c7506358d6e54
James Jallorina <james@deziretech.com>
2025-09-01 15:00:13 +0800

    Initial commit

---
+++ SolaceDemoApp/subscriber/src/main/java/com/example/demo/SubscriberApp.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberApp.class, args);
+	}
+
+}
